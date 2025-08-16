363493aa2b8572ebfaa1ca625cf7912e2777cb54
qixuema <qixuema@tencent.com>
2025-08-16 19:46:44 +0800

    Add chains_to_seam_mesh function to trimesh_utils.py for creating seam meshes from vertex chains
    - Introduced a new function `chains_to_seam_mesh` that generates a mesh from given vertices, faces, and chains.
    - Utilized `segments_to_prisms` to create chain meshes with random colors.
    - Enhanced the existing `save_seam_mesh` function with improved structure and organization.
    - Added necessary import for numpy to support new functionality.

--- src/seamutils/trimesh_utils.py
+++ src/seamutils/trimesh_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import trimesh
 from qixuema.trimesh_utils import segments_to_prisms
 
@@ -13,3 +14,32 @@ def save_seam_mesh(output_path, xyz, faces, seam_edges):
     )
     
     mesh.export(output_path)
+    
+
+def chains_to_seam_mesh(vertices, faces, chains, output_path='./'):
+    base_mesh = trimesh.Trimesh()
+    base_mesh.vertices = vertices
+    base_mesh.faces = faces
+
+    print(f'{len(chains)} chains')
+
+    meshes = []
+    meshes.append(base_mesh)
+    
+    for i, chain in enumerate(chains):
+        segments = np.stack([chain[:-1], chain[1:]], axis=1)
+        color = np.random.rand(3)
+
+        chain_mesh = segments_to_prisms(
+            segments,
+            color=color,
+        )
+
+        meshes.append(chain_mesh)
+
+    mesh = trimesh.util.concatenate(meshes)
+    
+    return mesh
+
+    
+    
